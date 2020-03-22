fb925a0282b28ca2f3f7a7839376b396ca9bd663
Ingolf Wagner <contact@ingolf-wagner.de>
2020-03-22 10:34:20 +0800

    small example that works

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 extern crate libpulse_binding as pulse;
 extern crate libpulse_simple_binding as psimple;
 
+extern crate minimp3;
+
+use minimp3::{Decoder, Error, Frame};
+
+use std::fs::File;
+
 use psimple::Simple;
 use pulse::sample;
 use pulse::stream::Direction;
 
 fn main() {
     let spec = sample::Spec {
-        format: sample::Format::F32le,
-        channels: 1,
+        format: sample::Format::S16le,
+        channels: 2,
         rate: 44100,
     };
     assert!(spec.is_valid());
@@ -25,24 +31,53 @@ fn main() {
     )
     .unwrap();
 
-    const BUFFER_SIZE: usize = 512;
-    const FREQUENCY: f32 = 440.0;
-    let mut t: f32 = 0.0;
-    let t_inc = 2.0 * std::f32::consts::PI * FREQUENCY / spec.rate as f32;
+    const BUFFER_SIZE: usize = 1152;
 
-    for _x in 0..300 {
-        let mut buffer: [f32; 2 * BUFFER_SIZE] = [0.0; 2 * BUFFER_SIZE];
-        for i in 0..BUFFER_SIZE {
-            //buffer[i] =  f32::sin(t ) ;
-            //buffer[i + BUFFER_SIZE] = buffer[i];
-            buffer[i * 2 + 0] = f32::sin(t);
-            buffer[i * 2 + 1] = f32::sin(t);
-            t = t + t_inc;
+    let mut decoder = Decoder::new(File::open("assets/sample1.mp3").unwrap());
+
+    loop {
+        match decoder.next_frame() {
+            Ok(Frame {
+                data,
+                sample_rate,
+                channels,
+                ..
+            }) => {
+                println!(
+                    "Decoded {} samples, {} channels with {} sample_rate",
+                    data.len() / channels,
+                    channels,
+                    sample_rate
+                );
+
+                let mut buffer: [i16; 2 * BUFFER_SIZE] = [0; 2 * BUFFER_SIZE];
+                let mut index = 0;
+                for sample_part in data.iter() {
+                    buffer[index] = *sample_part;
+                    index = index + 1;
+                }
+                let output_data: [u8; 2 * 2 * BUFFER_SIZE] = unsafe { std::mem::transmute(buffer) };
+                pulse_simple.write(output_data.as_ref()).unwrap();
+            }
+
+            Err(Error::Eof) => break,
+            Err(e) => panic!("{:?}", e),
         }
-        let data: [u8; 2 * 4 * BUFFER_SIZE] = unsafe { std::mem::transmute(buffer) };
-        pulse_simple.write(data.as_ref()).unwrap();
     }
 
     pulse_simple.drain().unwrap();
     pulse_simple.flush().unwrap();
+}
+
+fn f32_to_i16(f: f32) -> i16 {
+    // prefer to clip the input rather than be excessively loud.
+    (f.max(-1.0).min(1.0) * i16::max_value() as f32) as i16
+}
+
+/// Returns a 24 bit WAV int as an i16. Note that this is a 24 bit integer, not a
+/// 32 bit one. 24 bit ints are in the range [−8,388,608, 8,388,607] while i16s
+/// are in the range [-32768, 32767]. Note that this function definitely causes
+/// precision loss but hopefully this isn't too audiable when actually playing?
+fn i24_to_i16(i: i32) -> i16 {
+    (i >> 8) as i16
 }
